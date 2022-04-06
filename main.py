5770f8f9a4a4beafc0d4b7b65430a5f088b63b42
Onion-IO <66889399+Onion-IO@users.noreply.github.com>
2022-04-06 20:34:01 +0800

    Added lots of new features, and caught some errors
    Also added the profile picture for the bot's account.
    Added:
    HTML Help command
    Add to server command
    Some error messages

--- main.py
+++ main.py
@@ -13,24 +13,79 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author != client.user:  #ensures bot does not react to it's own messages
-        if message.content.startswith('&'):
+
+        if message.content == '&Thelp':
+            embed=discord.Embed(title="Help", color=0x87c0ac)
+            embed.set_author(name="TinPhone")
+            embed.add_field(name="-----------", value="-----------", inline=False)
+            embed.add_field(name="&Tmsg (DMs only)", value="Begins prompting for anonymous message sending", inline=False)
+            embed.add_field(name="&Tadd", value="Provides link to add this bot to your server", inline=False)
+            embed.add_field(name="&Thelp", value="Brings up this window", inline=False)
+            await message.channel.send(embed=embed)
+            return
+
+        if message.content == '&Tadd':
+            await message.channel.send('Use this link to add me to your server: https://discord.com/api/oauth2/authorize?client_id=960824698160185364&permissions=274878131200&scope=bot')
+            return
+
+
+        if message.content.startswith('&Tmsg'):
             if message.channel.type == discord.ChannelType.private:
                 await message.channel.send('Which server would you like to find?')
 
 
-                def pred(msg):
-                    return message.author.id == msg.author.id   #makes sure message comes from right person
+                def pred(srv):
+                    return message.author.id == srv.author.id   #makes sure message comes from right person
                 try: 
-                    msg = await client.wait_for("message", check=pred, timeout=20) #waits for message
+                    srv = await client.wait_for("message", check=pred, timeout=20) #waits for message
                 except asyncio.TimeoutError:   #catches timeout error
                     pass 
                 else:
-                    print(msg.content)
-                    user = msg.author 
-                    print("found mutual:", discord.utils.get(user.mutual_guilds, name = msg.content)) #checks mutual guilds for guild of name msg.content
+                    print("looking for: ", srv.content)
+                    user = srv.author 
+                    print("found mutual:", discord.utils.get(user.mutual_guilds, name = srv.content)) #checks mutual guilds for guild of name msg.content
+                    mutualserver = discord.utils.get(user.mutual_guilds, name = srv.content)  #logs mutual guild as 'mutualserver'
+                    if mutualserver == None:
+                            await message.channel.send("Sorry, I couldn't find that! Please use '&Tmsg' again") #If utils.get returns nothing, returns
+                            return
                     await message.channel.send('Which channel would you like to speak in?')
-                    return
+                    
 
+
+                    def pred(chn):
+                        return message.author.id == chn.author.id
+                    try: 
+                        chn = await client.wait_for("message", check=pred, timeout=20) 
+                    except asyncio.TimeoutError:   
+                        pass 
+                    else:
+                        print("looking for: ", chn.content) #Logs reply
+                        ServChn = discord.utils.get(mutualserver.text_channels, name=chn.content)  #Finds 'chn.content' in 'mutualserver' and logs as 'ServChn'
+                        if ServChn == None:
+                            await message.channel.send("Sorry, I couldn't find that! Please use '&Tmsg' again") #If utils.get returns nothing, returns
+                            return
+                        print("found: ", ServChn)
+                        await message.channel.send('What is your message?')
+
+
+
+                        def pred(msg):
+                            return message.author.id == msg.author.id
+                        try: 
+                            msg = await client.wait_for("message", check=pred, timeout=20) 
+                        except asyncio.TimeoutError:   
+                            pass 
+                        else:
+                            print(msg.content)
+                            await ServChn.send(msg.content)
+                            await message.channel.send('Message sent!')
+                            return
+
+
+                    return
+            else:
+                await message.channel.send('Woops! That command has to be used in DMs only.')
+    
     else:
         return
 
